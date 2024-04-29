acc38e7a3e75880e4c5091f834373652c1c6ffa5
Patrick Willians Powel <patrickwillianspowel@gmail.com>
2024-04-29 20:35:30 -0300

    minhas_tarefas

---
+++ src/components/Tarefa/newFile.ts
@@ -0,0 +1,10 @@
+import * as S from './styles';
+
+<>/S.Titulo>/
+  < S.Tag > importante < /S.Tag>/
+  < S.Tag > pendente < /S.Tag>/
+  < S.Descricao /  >
+  <S.BarraAcoes><S.Botao>Editar < /S.Botao>/
+  < S.Botao > Remover < /S.Botao>/
+  < /S.BarraAcoes>/
+  < /S.Card>/;
